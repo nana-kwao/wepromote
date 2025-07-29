bbce2adc3f0f7de686c922345e3042eec7155b92
nana-kwao <nanakwaofreiku@gmail.com>
2025-07-29 19:08:19 +0000

    minor update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 function App() {
   return (
     <>
       <h1>Coming Soon</h1>
+      <AiOutlineLoading3Quarters />
     </>
   );
 }
